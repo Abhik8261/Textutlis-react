19c550cdd47b2e6b9135353680766c99d18a90a0
Abhik8261 <abhik.tech8261@gmail.com>
2022-09-18 01:01:04 +0530

    Updates

---
+++ precache-manifest.0ff59a0ac79b338ba1c742e45b157154.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "89a4607b10aa7bdef4eb",
+    "url": "/static/css/main.b20249ea.chunk.css"
+  },
+  {
+    "revision": "89a4607b10aa7bdef4eb",
+    "url": "/static/js/main.89a4607b.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "fc6bf2a49d77b21506bb",
+    "url": "/static/js/2.fc6bf2a4.chunk.js"
+  },
+  {
+    "revision": "7ee0e2da555c3fd35366",
+    "url": "/static/js/runtime~main.7ee0e2da.js"
+  },
+  {
+    "revision": "c663647299fbb4ecd8c5012f332030d3",
+    "url": "/index.html"
+  }
+];
